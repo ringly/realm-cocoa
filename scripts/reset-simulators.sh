f323b2958b62f64cc9a27fe8de7914bc1e841190
JP Simard <jp@jpsim.com>
2016-09-09 16:53:53 -0700

    use jq to parse simctl

--- scripts/reset-simulators.sh
+++ scripts/reset-simulators.sh
@@ -13,45 +13,22 @@ while pgrep -q Simulator; do
     pkill -9 Simulator 2>/dev/null || true
 done
 
-# Shut down simulators until there's no booted ones left
-# Only do one at a time because devices sometimes show up multiple times
-while xcrun simctl list | grep -q Booted; do
-  xcrun simctl list | grep Booted | sed 's/.* (\(.*\)) (Booted)/\1/' | head -n 1 | xargs xcrun simctl shutdown
+# Install jq if needed
+if ! which jq >/dev/null; then
+    brew install jq
+fi
+
+# Shut down booted simulators
+xcrun simctl list devices -j | jq -c -r '.devices | flatten | map(select(.availability == "(available)")) | map(select(.state == "Booted")) | map(.udid) | .[]' | while read udid; do
+    echo "shutting down simulator with ID: $udid"
+    xcrun simctl shutdown $udid
 done
 
-# Clean up all available simulators
-(
-    previous_device=''
-    IFS=$'\n' # make newlines the only separator
-    for LINE in $(xcrun simctl list); do
-        if [[ $LINE =~ unavailable || $LINE =~ disconnected ]]; then
-            # skip unavailable simulators
-            continue
-        fi
-
-        if [[ $LINE =~ "--" ]]; then
-            # Reset the last seen device so we won't consider devices with the same name to be duplicates
-            # if they appear in different sections.
-            previous_device=""
-            continue
-        fi
-
-        regex='^(.*) [(]([0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12})[)]'
-        if [[ $LINE =~ $regex ]]; then
-            device="${BASH_REMATCH[1]}"
-            guid="${BASH_REMATCH[2]}"
-
-            # Delete the simulator if it's a duplicate of the last one seen
-            # Otherwise delete all contents and settings for it
-            if [[ $device == $previous_device ]]; then
-                xcrun simctl delete $guid
-            else
-                xcrun simctl erase $guid
-                previous_device="$device"
-            fi
-        fi
-    done
-)
+# Erase all available simulators
+xcrun simctl list devices -j | jq -c -r '.devices | flatten | map(select(.availability == "(available)")) | map(.udid) | .[]' | while read udid; do
+    echo "erasing simulator with ID: $udid"
+    xcrun simctl erase $udid
+done
 
 if [[ -a "${DEVELOPER_DIR}/Applications/Simulator.app" ]]; then
     open "${DEVELOPER_DIR}/Applications/Simulator.app"

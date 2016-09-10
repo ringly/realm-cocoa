12e1a14988d24a231cd7ff9a5d57ecbb041029c3
JP Simard <jp@jpsim.com>
2016-09-09 18:16:56 -0700

    log when running job again

--- build.sh
+++ build.sh
@@ -928,7 +928,7 @@ EOM
             failed=0
             sh build.sh verify-$target | tee build/build.log | xcpretty -r junit -o build/reports/junit.xml || failed=1
             if [ "$failed" = "1" ] && cat build/build.log | grep -E 'XTXProxyChannel|DTXChannel'; then
-                # Run again if a known Xcode error occurs
+                echo "Known Xcode error detected. Running job again."
                 failed=0
                 sh build.sh verify-$target | tee build/build.log | xcpretty -r junit -o build/reports/junit.xml || failed=1
             fi
